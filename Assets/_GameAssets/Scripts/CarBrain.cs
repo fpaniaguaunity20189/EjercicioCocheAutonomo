637381286f2beb109155b1daad67dcbbe990711e
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2018-10-04 12:06:09 +0200

    Initial commit

---
+++ Assets/_GameAssets/Scripts/CarBrain.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBrain : MonoBehaviour {
+    [SerializeField] int speed = 5;
+    [SerializeField] int speedRotation = 5;
+    int marcha = 1;
+	
+	void Update () {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * marcha);
+        if (marcha == -1)
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * speedRotation);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Pared")
+        {
+            //He colisionado contra la pared
+            marcha = marcha * -1;
+        }
+        if (collision.gameObject.tag == "Target")
+        {
+            //He encontrado el objetivo, me detengo
+            Debug.Log("FERNANDO HA ENCONTRADO EL OBJETIVO");
+            marcha = 0;
+        }
+    }
+}
---
+++ Assets/_GameAssets/Scripts/Coche.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coche : MonoBehaviour {
+
+    [SerializeField] int speed = 20;
+    [SerializeField] int speedRotation = 12;
+    float marcha = 1;
+    int ranRot;
+	
+	void Update () {
+        transform.Translate (Vector3.forward * Time.deltaTime * speed * marcha);
+        if (marcha == -1)
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * speedRotation);
+        }
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag ("Pared"))
+        {
+            marcha = marcha * -1;
+            ranRot = Mathf.RoundToInt (Random.Range(-12, 12));
+            speedRotation = Random.Range (speedRotation, speedRotation + 5 * ranRot);
+        }
+        if (collision.gameObject.CompareTag("Target"))
+        {
+            speed= 0;
+            Debug.Log("JORGE HA ENCONTRADO EL OBJETIVO");
+        }
+    }
+}
